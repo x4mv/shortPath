b10a8f83bc8a486aecf5ddac653a8a26d67bdc94
x4mv <contactsscu@gmail.com>
2025-03-31 13:08:59 -0300

    creando la grilla

--- shortPath/src/views/Grid.tsx
+++ shortPath/src/views/Grid.tsx
@@ -1,11 +1,24 @@
 import "../index.css";
 
+const rows = 3;
+const cols = 3;
+const totalCells = rows * cols 
+
 
 const Grid = () => {
   return (
 
-    <div>
-        grid
+    <div className="grid grid-cols-3 gap-4 p-5">
+        {[...Array(totalCells)].map((_,index) =>(
+            <div
+            key={index}
+            id={`cell-${index+1}`} // unique ID
+            className="bg-blue-500 text-white p-5 text-center font-bold"
+            
+            >
+                Cell {index+1}
+            </div>
+        ))}
     </div>
   )
 }
